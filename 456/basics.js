564fcca7819e1d6da5388d4a51b4e9f18bc00d44
Star890 <rosalynj999@hotmail.com>
2019-04-25 09:03:10 -0500

    Add files via upload

---
+++ 456/basics.js
@@ -0,0 +1,301 @@
+
+//Create an array to represent your shopping list with the following items: 'pop tarts', 'ramen noodles', 'chips', 'salsa', and 'coffee'.
+//Add 'fruit loops' to the list.
+//Update 'coffee' to 'fair trade coffee'
+///Replace 'chips' and 'salsa' with 'rice' and 'beans'
+//Create an empty array to represent your shopping cart.
+//Remove the last item from your shopping list and add it to your cart
+//Remove the first item from your shopping list and add it to the cart
+//Write a 'while' loop that takes items from your shopping list and moves them to your cart until there are no items left on the list.
+//Sort the items in your cart alphabetically... backwards
+//Print the list of items in your shopping cart to the console as comma separated string.
+
+
+var course = {
+	name: 'JavaScript Applications',
+	awesome: true,
+	teachers: ['Assaf', 'Shane'],
+	students: [
+		{
+			name: 'Steve',
+			computer: {
+				OS: 'Linux',
+				type: 'laptop'
+			}
+		},
+		{
+			name: 'Katy',
+			computer: {
+				OS: 'OSX',
+				type: 'macbook'
+			}
+		},
+		{
+			name: 'Chuck',
+			computer: {
+				OS: 'OSX',
+				type: 'macbook'
+			}
+		}
+
+	],
+	preReqs : {
+		skills : ['html', 'css', 'git'],
+		equipment: {
+			laptop: true,
+			OSOptions: ['linux', 'osx']
+		}
+
+
+
+
+console.log(course.name);
+console.log(course.teachers.[1]);
+console.log (course.students.[0].name);
+console.log (course.students.[1].computer.type);
+console.log (course.preReqs.equipment.OSOptions[1]);
+
+console.log (course.preReqs.equipment.OSOptions.join ("or"));	
+ var students = ("katy", "chuck");
+ var OSOptions: ('linux', 'osx');
+	
+var kings_arr= [];
+course.students.map(student=>){
+	if (student.computer.os==="osx"){
+		kings_arr.push(student.name);
+	}
+}
+console.log(kings_arr);
+
+
+var blake_arr =[]
+for(var i-0; i < course.students.legnth; i++){
+	if (course.students[i].computer.os === "OSX"){
+		blake_arr.push (course.students[i].name)
+	}
+
+console.log (blake_arr);
+
+
+
+
+
+var name1= prompt ("what is your name");
+var school= prompt ("what school are you attending");
+var laptop= prompt ("what laptop are you currenly using");
+var legnth_of_school= prompt("how long is left of your schooling");
+
+function studentguide() {
+	conslole.log("you attend the school of " + school);
+}
+studentguide()
+------------
+var pink = ["Rosalyn","Mayor","Jeremy"];
+console.log[2](pink); 	
+
+
+
+
+
+// ---------------------------------------------------------------------------------
+
+
+// var shoppinglist = [ "pop tarts", "ramen noodles", "chips", "salsa", "coffee"];
+// shoppinglist.push('fruitloops')
+// console.log(shoppinglist);
+// shoppinglist[4] = "fair trade coffee"
+// shoppinglist[2]= "rice"
+// shoppinglist[3]= "beans"
+//  var shoppingcart= []
+// shoppingcart.push (shoppinglist.pop());
+// shoppingcart.push (shoppinglist.shift());
+
+// while ( shoppinglist.length>0){
+// 	shoppingcart.push(shoppinglist.pop())
+// }
+
+// shoppingcart.sort().reverse();
+
+// console.log(shoppingcart);
+// ----------------------------------
+// Creating an Object 
+// var name_of_object = {
+// 	key: value
+// }
+// ------
+// var dog = {
+// 	name: "leo",
+// 	speed: "100",
+// 	stamina: 80,
+// 	coat:"black&tan",
+// 	nails: "short"
+// 	health: 100,
+
+
+
+// vet: {
+// 		name: "charles"
+// 		contact: "281-330-8080"
+// 		clinic: 
+// 		profession: "lead vet"
+// 		specialization: "dogs"
+// 	}
+// }
+
+// siblings: ["lucy", "winter", "larry"]
+// console.log(dog.name, dog.coat, siblings)
+
+// console.log( dog, ownner, dog, vet, clinic)
+
+// console.log(dog.owner.vet);
+
+// dog.speed= 100;
+// console.log ("your dogs speed is" + dog.speed);
+
+
+
+
+
+
+
+
+
+
+// console.log(course.prereqs.equipment);
+
+
+
+
+
+
+
+
+
+// teachers.reverse();   do it once will reverse
+// teachers.reverese()		type again will put it back 
+// console.log(teachers);
+
+// will sort information values in string from a-z and 0-largest #
+
+// teachers.sort()
+// console.log(teachers);
+
+// var cupsofcoffee = 5
+
+// if ( cupsofcoffee < 5) {
+// 	console.log( "Buy another coffee!");
+
+// }
+// else { console.log ( "Dont buy another coffee");
+
+// }
+
+// var user_answer= prompt ("what is your favorite bread");
+
+// console.log("your favorite bread is ms.brairds" + user_answer);
+
+
+
+
+// let cups = 1
+
+// if (cups< 3) {
+// 	console.log ("yes. Ill take another");
+
+// }
+
+// else {
+// 	console.log("I think Im ok for now");
+// }
+
+
+
+
+
+
+// var indoors = true;
+// var temp=80;
+// console.log ( the tempature is 80 )
+
+
+
+// console.log ("the temp is " + temp + "degrees"); {
+// 	if (indoors === true) {
+// 		console.log ("time to swim");
+// }
+
+// var precipitation = false;
+
+// if ( tempature> 80 && precipitation === false) {
+// 	console.log( "time to swim");
+	
+// }
+
+// else if ( indoors === true) {
+// 	console.log ("time to swim");
+// }
+
+
+// //* FOR LOOPS//
+
+// let bottle = 99;
+
+// for (bottle; bottle >= 0; bottle--);
+//  if (bottle ==== 0) {}
+
+
+
+
+
+
+
+
+// var game = "Would you like to play a game?"
+// var name = prompt("What is your name?"); {
+    
+//     console.log("What's up", name + "?" + " " + game + "?")
+// }
+
+
+
+
+// var user_answer= prompt ("rock || paper || scissors");
+// console.log ( "user_answer"); 
+
+// if ( rock === rock) {
+// 	console.log ( rock = true);
+// }
+// if ( paper === paper) {
+// 	console.log( paper= true);
+// }
+// if ( scissors=== scissors)
+
+
+// for ( var rock= true; i <10; i++1){
+// 	console.log(x +1 );
+// }
+
+
+
+// 	}
+// }
+
+
+
+
+
+
+
+
+// var correctanswer = prompt ( "do you want to build a snowman?");
+// while (correctanswer != "yes") { 
+// 	var correctanswer = prompt ("do you want to build a snowman?")
+// 	if  (correctanswer === "yes")
+// 		console.log ("yay! lets go to disney!");
+
+// } 
+
+
+
+
+
